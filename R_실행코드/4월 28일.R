2d9fad0776d5ff6c7a05d2072dc2f275557e5fe8
KANGJUGWANG <djenak01@gmail.com>
2025-04-28 13:28:15 +0900

    활용할 데이터셋 추가

---
+++ R_실행코드/4월 28일.R
@@ -0,0 +1,19 @@
+##오늘자 강의 내용 
+#1. 데이터 제공 사이트에 접속하여 제공 형식과 일치한 데이터를 다운받은후 검사
+#2. 데이터 형식
+#2.1 변수 A(명목형), 변수 B(명목형)
+#2.2 변수 A(연속형), 변수 B(명목형), 변수 C(명목형)
+#3. 제공 사이트
+"
+Kaggle : https://www.kaggle.com/
+  • Google Dataset Search : https://toolbox.google.com/datasetsearch
+• UCI Machine Learning Repository : https://archive.ics.uci.edu/
+  • Data.gov : https://data.gov/
+  • Naver DataLab : https://datalab.naver.com/
+  • Huggingface : https://huggingface.co/spaces
+"
+
+## 선택 데이터셋 
+# 명목형 2 : 국경통과 데이터셋(추출)
+# 명목형 2, 연속형 1 : 전자상거래 데이터셋(추출)
+# 끝
